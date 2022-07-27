a5a9075ab6e9caac11c64b444613c9aaa0a8f257
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-07-26 22:24:20 -0700

    fixed manager_id error

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ('Managerial'),
+       ('Engineering'),
+       ('Sales'),
+       ('HR'),
+       ('Communication');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Engineer', 100000.00, 2),
+       ("Intern", 0.00, 5),
+       ("Manager", 200000.00, 1),
+       ("Sales Person", 120000.00, 3),
+       ("Comms guy", 70000.00, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Billy', 'Bob', 5, 1),
+       ("I sell", 'things', 4, 1),
+       ("Jerome", "Manages", 3, null),
+       ("I bust", "code", 2, 1),
+       ("I fix", "busted code", 1, 2);
--- script.js
+++ script.js
@@ -2,7 +2,6 @@ require('.dotenv').config();
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-
 const db = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
@@ -11,3 +10,27 @@ const db = mysql.createConnection({
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+function chooseFunction() {
+    inquirer.prompt(
+      [
+      {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'choice',
+        choices: ['View All Departments', 'View All Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
+      },
+      ]
+    ).then((functionChoice) => {
+        if (functionChoice.choice === 'View All Departments') {
+            db.query('SELECT * FROM employees', function (err, results) {
+                console.log(results);
+              });
+        } else if (functionChoice.choice === 'View All Employees') {
+            db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
+                console.log(results);
+              });
+        }
+    }     
+    )
+}
