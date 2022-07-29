f64d806a7ab741615209fd5cdb7ced79d2de28a9
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-07-28 22:31:34 -0700

    added getRoles, getEmployees()

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+const mysqlPromise = require("mysql2/promise");
+
+
+ function getRoles() {
+   db.query("SELECT * FROM role", function (err, results) {
+    if (results) {
+      results.forEach(function (role) {
+        roles.push(role.title);
+      });
+    }
+  });
+  return roles;
+}
+
 const db = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
@@ -16,36 +30,32 @@ var departments = [];
 var roles = [];
 var employees = ["None"];
 
-db.query(
-  `SELECT role.id, role.title, department.name AS department, role.salary FROM role INNER JOIN department ON role.department_id=department.id`,
-  function (err, results) {
-    console.log("\n");
-    console.table(results);
-  }
-);
+ function chooseFunction() {
+  roles = getRoles();
+  employees = getEmployees();
+  departments = getDepartmentNames();
 
-function chooseFunction() {
-    inquirer.prompt(
+     inquirer.prompt(
       {
         type: 'list',
         message: 'What would you like to do?',
         name: 'choice',
-        choices: ['View All Departments', 'View All Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Quit']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Quit']
       },
     ).then((functionChoice) => {
         if (functionChoice.choice === 'View All Departments') {
-            db.query('SELECT * FROM department', function (err, results) {
+             db.query('SELECT * FROM department', function (err, results) {
                 console.table(results);
               });
               chooseFunction();
 
         } else if (functionChoice.choice === 'View All Roles') {
-          db.query('SELECT * FROM role', function (err, results) {
+           db.query('SELECT * FROM role', function (err, results) {
               console.table(results);
             });
             chooseFunction();
         } else if (functionChoice.choice === 'View All Employees') {
-            db.query('SELECT * FROM employees', function (err, results) {
+             db.query('SELECT * FROM employee', function (err, results) {
                 console.table(results);
               });
               chooseFunction();
@@ -54,15 +64,135 @@ function chooseFunction() {
         } else if (functionChoice.choice === 'Add a Role') {
             addRole();
         } else if (functionChoice.choice === 'Add an Employee') {
-
+          addEmployee();
         } else if (functionChoice.choice === ' Update an Employee Role') {
-
-        } else {
+          updateEmployeeRole();
+        } else if (functionChoice.choice === 'Quit') {
           console.log("Goodbye!~");
           return;
         }
     }     
     )
+}
+
+function updateEmployeeRole() {
+  const questions = [
+    {
+      name: "employee_id",
+      message: "Which employee's role do you want to update?",
+      type: "list",
+      choices: employees,
+    },
+    {
+      name: "employee_role",
+      message: "Which role do you want to assign the selected employee?",
+      type: "list",
+      choices: roles,
+    },
+  ];
+  inquirer.prompt(questions).then((answers) => {
+    db.query(
+      `SELECT id FROM employee WHERE first_name="${
+        answers.employee_id.split(" ")[0]
+      }" AND last_name="${answers.employee_id.split(" ")[1]}"`,
+      function (err, results) {
+        console.log("\n");
+        employeeId = results[0].id;
+        connection.query(
+          `SELECT id FROM role WHERE title="${answers.employee_role}"`,
+          function (err, results) {
+            console.log("\n");
+            console.log(results);
+            id = results[0].id;
+            connection.query(
+              `UPDATE employee SET role_id='${id}' WHERE id='${employeeId}'`,
+              function (err, results) {
+                console.log(
+                  `\n Updated ${
+                    answers.employee_id.split(" ")[0]
+                  } to the employee table`
+                );
+              }
+            );
+          }
+        );
+      }
+    );
+    chooseFunction();
+  });
+}
+
+function addEmployee() {
+  const questions = [
+    { name: "first_name", message: "What is the employee's first name?" },
+    { name: "last_name", message: "What is the employee's last name?" },
+    {
+      name: "role",
+      message: "What is the employee's role?",
+      type: "list",
+      choices: roles,
+    },
+    {
+      name: "manager_id",
+      message: "Who is the employee's manager?",
+      type: "list",
+      choices: employees,
+    },
+  ];
+  inquirer.prompt(questions).then((answers) => {
+    db.query(
+      `SELECT id FROM role WHERE title="${answers.role}"`,
+      function (err, results) {
+        id = results[0].id;
+        if (answers.manager_id === "None") {
+          connection.query(
+            `INSERT INTO employee (first_name, last_name, role_id) VALUES ("${answers.first_name}","${answers.last_name}","${id}")`,
+            function (err, results) {
+              console.log(
+                `\n Added ${answers.first_name} to the employee table`
+              );
+            }
+          );
+        } else {
+          addEmployeeWManager(answers, id);
+        }
+      }
+    );
+    // employees = getEmployees();
+    chooseOption();
+    // break;
+  });
+}
+
+function getEmployees() {
+  employees = ["None"];
+   db.query("SELECT * FROM employee", function (err, results) {
+    if (results) {
+      results.forEach(function (employee) {
+        employees.push(`${employee.first_name} ${employee.last_name}`);
+      });
+    } else {
+      console.error(err);
+    }
+  });
+  return employees;
+}
+
+function addEmployeeWManager(answers, roleId) {
+  db.query(
+    `SELECT id FROM employee WHERE first_name="${
+      answers.manager_id.split(" ")[0]
+    }" AND last_name="${answers.manager_id.split(" ")[1]}"`,
+    function (err, results) {
+      employeeId = results[0].id;
+      connection.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${`"${answers.first_name}","${answers.last_name}",${roleId}, ${employeeId}`})`,
+        function (err, results) {
+          console.log(`\n Added ${answers.first_name} to the emloyee table`);
+        }
+      );
+    }
+  );
 }
 
  function addDepartment() {
@@ -89,7 +219,7 @@ function getDepartmentNames() {
   return departments;
 }
 
-function addRole () {
+ function addRole () {
   // getDepartmentNames()
   const roleQuestions = [{
     type: 'input',
@@ -109,11 +239,22 @@ function addRole () {
     choices: getDepartmentNames()
   }
 ];
-  inquirer.prompt(roleQuestions).then((roleInfo) => {
+
+
+   inquirer.prompt(roleQuestions).then((roleInfo) => {
     db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${roleInfo.roleName}', '${roleInfo.roleSalary}', '${roleInfo.roleDepartment.id}');`, function (err, results) {
     });
     chooseFunction();
   } );
+
 }
 
-chooseFunction();
+// chooseFunction();
+function init() {
+  roles = getRoles();
+  employees = getEmployees();
+  departments = getDepartmentNames();
+  chooseFunction();
+}
+
+init();
